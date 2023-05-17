8848450c2236f2e67a08511bd42d93a7af193c71
DanielRiha8906 <129500714+DanielRiha8906@users.noreply.github.com>
2023-05-17 23:19:46 +0200

    Kamen and Spike update

--- kamen.py
+++ kamen.py
@@ -1,16 +1,20 @@
 class Kamen:    
 
     def __init__(self, barva, nazev):
-        self.barva = barva
+        self._barva = barva
         self.pamet_pohybu = []  # Seznam polí, na kterých se kámen nacházel (včetně aktuálního)
-        self.nazev = nazev
-    def get_barva(self):    
-        return self.barva
-    #Vrací aktuální pole, na kterém se kámen nachází.
+        self._nazev = nazev
     
+    @property 
+    def barva(self):    
+            return self._barva
+
+
+    #Vrací aktuální pole, na kterém se kámen nachází.
     def get_pole(self):
         return self.pamet_pohybu[-1]
+
+
     #Vrací seznam polí, na kterých se kámen nacházel (včetně aktuálního).
-    
     def get_pamet_pohybu(self):
         return self.pamet_pohybu
--- spike.py
+++ spike.py
@@ -2,18 +2,22 @@ from kamen import Kamen
 
 class Spike:
     def __init__(self, spike_id):
-        self.spike_barvy = spike_barvy
-        self.kameny = []
+        self._spike_barvy = None
+        self._kameny = []
 
+    @property
+    def kameny(self):
+        return self._kameny
     def pridej_kamen(self,spike_id, Kamen):
-        self.kameny.append(Kamen)
+        self._kameny.append(Kamen)
 
     def pocet_kamenu(self, spike_id):
-        return len(self.kameny)
+        return len(self._kameny)
 
     def je_prazdny(self, spike_id):
-        return len(self.kameny) == 0
+        return len(self._kameny) == 0
+
     def odeber_kamen(self, spike_id, Kamen):
-        self.kameny.remove(Kamen)
+        self._kameny.remove(Kamen)
         print("Kamen je pryč")
         
