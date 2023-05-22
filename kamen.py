6f09199e4af94e8fa8c944d98865e49fab730341
Přemysl Hruška <premekhruska280@gmail.com>
2023-05-22 19:10:16 +0200

    Vyvorena metoda pro vytvareni kamenu

--- kamen.py
+++ kamen.py
@@ -1,52 +1,34 @@
-class Kamen:    
+class Kamen: 
+    seznam = []   
 
-    def __init__(self, barva, nazev):
-        self.barva = barva
-        self.pole = None  # Aktuální pole, na kterém se kámen nachází
+    def __init__(self, barva,kamen_id):
+        self._barva = barva
         self.pamet_pohybu = []  # Seznam polí, na kterých se kámen nacházel (včetně aktuálního)
-        self.nazev = nazev
+        self.kamen_id = kamen_id
+    
+    @property 
+    def barva(self):    
+            return self._barva
 
-    def get_barva(self):
-        
-        return self.barva
 
+    #Vrací aktuální pole, na kterém se kámen nachází.
     def get_pole(self):
-        """Vrací aktuální pole, na kterém se kámen nachází."""
-        return self.pole
+        return self.pamet_pohybu[-1]
+    
+    def __str__(self):
+        return f"kamen_{self.barva}{self.kamen_id}"
 
+
+    #Vrací seznam polí, na kterých se kámen nacházel (včetně aktuálního).
     def get_pamet_pohybu(self):
-        """Vrací seznam polí, na kterých se kámen nacházel (včetně aktuálního)."""
         return self.pamet_pohybu
     
+    def vytvor_kameny():
+        for i in range(1,16):
+            kamen_white = Kamen("white",i)
+            Kamen.seznam.append(kamen_white)
+        for i in range (1,16):
+            kamen_black = Kamen("black",i)
+            Kamen.seznam.append(kamen_black)
 
-kamen1_cerny = Kamen("cerny", " C")
-kamen2_cerny = Kamen("cerny", " C")
-kamen3_cerny = Kamen("cerny", " C")
-kamen4_cerny = Kamen("cerny", " C")
-kamen5_cerny = Kamen("cerny", " C")
-kamen6_cerny = Kamen("cerny", " C")
-kamen7_cerny = Kamen("cerny", " C")
-kamen8_cerny = Kamen("cerny", " C")
-kamen9_cerny = Kamen("cerny", " C")
-kamen10_cerny = Kamen("cerny", " C")
-kamen11_cerny = Kamen("cerny", " C")
-kamen12_cerny = Kamen("cerny", " C")
-kamen13_cerny = Kamen("cerny", " C")
-kamen14_cerny = Kamen("cerny", " C")
-kamen15_cerny = Kamen("cerny", " C")
-
-kamen1_bily = Kamen("bily"," B")
-kamen2_bily = Kamen("bily"," B")
-kamen3_bily = Kamen("bily"," B")
-kamen4_bily = Kamen("bily"," B")
-kamen5_bily = Kamen("bily"," B")
-kamen6_bily = Kamen("bily"," B")
-kamen7_bily = Kamen("bily"," B")
-kamen8_bily = Kamen("bily"," B")
-kamen9_bily = Kamen("bily"," B")
-kamen10_bily = Kamen("bily"," B")
-kamen11_bily = Kamen("bily"," B")
-kamen12_bily = Kamen("bily"," B")
-kamen13_bily = Kamen("bily"," B")
-kamen14_bily = Kamen("bily"," B")
-kamen15_bily = Kamen("bily"," B")
+Kamen.vytvor_kameny()
